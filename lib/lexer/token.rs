b92c60a11d011790eba3d3db16b406451c768cc2
Brian Teague <br.teague@gmail.com>
2023-08-19 21:40:30 -0400

    Restore parser

--- lib/lexer/token.rs
+++ lib/lexer/token.rs
@@ -1,4 +1,12 @@
-#[derive(Debug, PartialEq)]
+use std::iter::Enumerate;
+use nom::{
+    InputIter,
+    InputTake,
+    Needed, 
+    InputLength
+};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Token<'a> {
     IntLiteral(&'a str),
     LeftParen,
@@ -8,30 +16,83 @@ pub enum Token<'a> {
     EOF,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Tokens<'a> {
-    tokens: &'a [Token<'a>],
-    index: usize,
+    pub tok: &'a [Token<'a>],
+    pub start: usize,
+    pub end: usize,
 }
 
 impl<'a> Tokens<'a> {
     pub fn new(vec: &'a [Token]) -> Self {
         Tokens {
-            tokens: vec,
-            index: 0
+            tok: vec,
+            start: 0,
+            end: vec.len(),
         }
     }
 }
 
-impl<'a> Iterator for Tokens<'a> {
+impl<'a> InputLength for Tokens<'a> {
+    #[inline]
+    fn input_len(&self) -> usize {
+        self.tok.len()
+    }
+}
+
+impl<'a> InputIter for Tokens<'a> {
     type Item = &'a Token<'a>;
+    type Iter = Enumerate<::std::slice::Iter<'a, Token<'a>>>;
+    type IterElem = ::std::slice::Iter<'a, Token<'a>>;
 
     #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.tokens.len() {
-            self.index += 1;
-            return Some(&self.tokens[self.index - 1]);
+    fn iter_indices(&self) -> Enumerate<::std::slice::Iter<'a, Token<'a>>> {
+        self.tok.iter().enumerate()
+    }
+    #[inline]
+    fn iter_elements(&self) -> ::std::slice::Iter<'a, Token<'a>> {
+        self.tok.iter()
+    }
+    #[inline]
+    fn position<P>(&self, predicate: P) -> Option<usize>
+    where
+        P: Fn(Self::Item) -> bool,
+    {
+        self.tok.iter().position(predicate)
+    }
+    #[inline]
+    fn slice_index(&self, count: usize) -> Result<usize, Needed> {
+        if self.tok.len() >= count {
+            Ok(count)
+        } else {
+            Err(Needed::Unknown)
         }
-        return None;
+    }
+}
+
+impl<'a> InputTake for Tokens<'a> {
+    #[inline]
+    fn take(&self, count: usize) -> Self {
+        Tokens {
+            tok: &self.tok[0..count],
+            start: 0,
+            end: count,
+        }
+    }
+
+    #[inline]
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (prefix, suffix) = self.tok.split_at(count);
+        let first = Tokens {
+            tok: prefix,
+            start: 0,
+            end: prefix.len(),
+        };
+        let second = Tokens {
+            tok: suffix,
+            start: 0,
+            end: suffix.len(),
+        };
+        (second, first)
     }
 }
